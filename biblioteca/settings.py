0adb219288c74db7178e7d984ac64f74e4ccd2f8
Randú <134299499+joserandu@users.noreply.github.com>
2025-06-17 16:55:44 -0300

    Debug false

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sai+9++glpo#*+mj)y7jo!r93ahib%2rv(6gx64uq0x+_jpyo5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
